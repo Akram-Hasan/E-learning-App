dafe75394969ac97a844cad177e63d9dec6b884d
Akram Hasan <akramhasan.xyz@gmail.com>
2024-05-05 22:17:16 +0530

    api fixed

--- App.js
+++ App.js
@@ -19,7 +19,7 @@ export default function App() {
   const [fontsLoaded, fontError] = useFonts({
     outfit: require("./assets/fonts/Outfit-Regular.ttf"),
     "outfit-medium": require("./assets/fonts/Outfit-Medium.ttf"),
-    "outfit-bold": require("./assets/fonts/Outfit-Bold.ttf"),//assets\fonts\Outfit-Bold.ttf
+    "outfit-bold": require("./assets/fonts/Outfit-Bold.ttf"), //assets\fonts\Outfit-Bold.ttf
   });
 
   const [auth, setAuth] = useState(false);
